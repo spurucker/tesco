fe047e24a114e951b628193a0d786133ceb96e75
spurucker <spurucker@users.noreply.github.com>
2021-04-02 20:31:55 -0300

    Adding documentation (#3)
    * Adding documentation
    * Adding Readme documentation

--- src/main/java/com/tesco/domain/Statistics.java
+++ src/main/java/com/tesco/domain/Statistics.java
@@ -1,13 +1,18 @@
 package com.tesco.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@ApiModel
 @NoArgsConstructor
 @AllArgsConstructor
 public class Statistics {
+    @ApiModelProperty(notes = "number of times that the url was shorten")
     private long shortened;
+    @ApiModelProperty(notes = "number of times that the url was accessed")
     private long accessed;
 }
